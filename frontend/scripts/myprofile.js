5616f23ec0eae8f444d2403aae1b04e3c576200d
Ekaterina Domashova <katy.domashova@gmail.com>
2024-12-19 22:09:35 +0300

    add other users profile

---
+++ frontend/scripts/myprofile.js
@@ -0,0 +1,33 @@
+document.getElementById('open_add-exchange').addEventListener('click', (event) => {
+  document.getElementById('form_add-exchange').style.display = 'flex';
+  document.getElementById('form_add-exchange').style.position = 'fixed';
+  document.body.style.overflow = "hidden";
+});
+
+document.getElementById('close_add-exchange').addEventListener('click', (event) => { 
+  document.getElementById('form_add-exchange').style.display = 'none';
+  document.body.style.overflow = "auto";
+});
+
+document.getElementById('open_add-wanted').addEventListener('click', (event) => {
+  document.getElementById('form_add-wanted').style.display = 'flex';
+  document.getElementById('form_add-wanted').style.position = 'fixed';
+  document.body.style.overflow = "hidden";
+});
+
+document.getElementById('close_add-wanted').addEventListener('click', (event) => { 
+  document.getElementById('form_add-wanted').style.display = 'none';
+  document.body.style.overflow = "auto";
+});
+
+document.getElementById('open_add-mybook').addEventListener('click', (event) => {
+  document.getElementById('form_add-mybook').style.display = 'flex';
+  document.getElementById('form_add-mybook').style.position = 'fixed';
+  document.body.style.overflow = "hidden";
+});
+
+document.getElementById('close_add-mybook').addEventListener('click', (event) => { 
+  document.getElementById('form_add-mybook').style.display = 'none';
+  document.body.style.overflow = "auto";
+});
+
