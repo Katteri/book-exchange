fcbdcf15615fb0f966faf049cb2672b9a303f095
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2024-12-19 16:49:58 +0300

    try connect to db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,22 @@
 const Sequelize = require("sequelize");
-const Pool = require('pg').Pool
+// const Pool = require('pg').Pool
 
-module.exports = new Pool({
-    user: "postgres",
-    password: "user",
+// module.exports = new Pool({
+//     user: "postgres",
+//     password: "user",
+//     host: "localhost",
+//     port: 5432,
+//     database: "book_exchange"
+// })
+
+module.exports = new Sequelize('book_exchange', 'postgres', 'user', {
     host: "localhost",
-    port: 5432,
-    database: "book_exchange"
+    dialect: 'postgres',
+    operatorsAliases: 0,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 3000,
+        idle: 10000
+    }
 })
