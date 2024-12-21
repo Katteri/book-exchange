ebd092b10a5d0ea9c0c02054bfe4e5bafd1cd135
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2024-12-21 16:53:18 +0300

    Update bookRouter.js

--- backend/src/routes/bookRouter.js
+++ backend/src/routes/bookRouter.js
@@ -2,13 +2,13 @@ const express = require("express");
 const BookController = require("../controllers/bookController");
 const router = express.Router();
 
-router.get("/wanted/:user_id", BookController.getWantedBooks);
-router.get("/owned/:user_id", BookController.getOwnedBooks);
-
 router.post("/wanted/add", BookController.addBookToWanted);
 router.post("/owned/add", BookController.addBookToOwned);
 
 router.delete("/wanted/delete", BookController.deleteBookFromWanted);
 router.delete("/owned/delete", BookController.deleteBookFromOwned);
+
+router.get("/wanted/:user_id", BookController.getWantedBooks);
+router.get("/owned/:user_id", BookController.getOwnedBooks);
 
 module.exports = router;
