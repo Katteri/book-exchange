ebaea2795492853cdfa59bb48e6ae07787bde521
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2024-12-23 11:27:39 +0300

    Update auth.js

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -2,16 +2,15 @@ const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    if (!authHeader) {
-        return res.status(401).json({ message: "Authorization header missing" });
-    }
-    const token = authHeader.split(' ')[1];
+    const token = authHeader && authHeader.split(" ")[1];
     if (!token) {
-        return res.status(401).json({ message: "Token missing" });
+        console.error("Token not provided")
+        return res.status(401).send("Access denied. No token provided")
     }
+    console.log("Received Token:", token);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
-            console.error("Token verification error:", err);
+            console.error("Token verification error:", err.message);
             return res.status(403).json({ message: "invalid or expired token" })
         }
         req.user = user;
