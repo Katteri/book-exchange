ddc4ca53abdcfb4839c865565731da740784e4a3
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2024-12-24 18:49:25 +0300

    refractor

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,4 @@
 const Sequelize = require("sequelize");
-// const Pool = require('pg').Pool
-
-// module.exports = new Pool({
-//     user: "postgres",
-//     password: "user",
-//     host: "localhost",
-//     port: 5432,
-//     database: "book_exchange"
-// })
 
 module.exports = new Sequelize('book_exchange', 'db_user', 'postgres', {
     host: "localhost",
