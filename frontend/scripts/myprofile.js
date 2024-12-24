67befbfdc4c986438e54f95807737ad950ee431a
Ekaterina Domashova <katy.domashova@gmail.com>
2024-12-24 18:39:14 +0300

    fix header

--- frontend/scripts/myprofile.js
+++ frontend/scripts/myprofile.js
@@ -1,33 +1,39 @@
 document.getElementById('open_add-exchange').addEventListener('click', (event) => {
   document.getElementById('form_add-exchange').style.display = 'flex';
   document.getElementById('form_add-exchange').style.position = 'fixed';
+  document.getElementsByClassName('header')[0].style.display = 'none';
   document.body.style.overflow = "hidden";
 });
 
 document.getElementById('close_add-exchange').addEventListener('click', (event) => { 
   document.getElementById('form_add-exchange').style.display = 'none';
+  document.getElementsByClassName('header')[0].style.display = 'flex';
   document.body.style.overflow = "auto";
 });
 
 document.getElementById('open_add-wanted').addEventListener('click', (event) => {
   document.getElementById('form_add-wanted').style.display = 'flex';
   document.getElementById('form_add-wanted').style.position = 'fixed';
+  document.getElementsByClassName('header')[0].style.display = 'none';
   document.body.style.overflow = "hidden";
 });
 
 document.getElementById('close_add-wanted').addEventListener('click', (event) => { 
   document.getElementById('form_add-wanted').style.display = 'none';
+  document.getElementsByClassName('header')[0].style.display = 'flex';
   document.body.style.overflow = "auto";
 });
 
 document.getElementById('open_add-mybook').addEventListener('click', (event) => {
   document.getElementById('form_add-mybook').style.display = 'flex';
   document.getElementById('form_add-mybook').style.position = 'fixed';
+  document.getElementsByClassName('header')[0].style.display = 'none';
   document.body.style.overflow = "hidden";
 });
 
 document.getElementById('close_add-mybook').addEventListener('click', (event) => { 
   document.getElementById('form_add-mybook').style.display = 'none';
+  document.getElementsByClassName('header')[0].style.display = 'flex';
   document.body.style.overflow = "auto";
 });
 
